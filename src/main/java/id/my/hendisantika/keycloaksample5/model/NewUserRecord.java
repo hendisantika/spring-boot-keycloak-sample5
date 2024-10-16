f35a4a5bc3b7d84da8d36d1ca2041e707c21922a
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:16:04 +0700

    Add NewUserRecord Model🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaksample5/model/NewUserRecord.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.keycloaksample5.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/10/24
+ * Time: 14.15
+ * To change this template use File | Settings | File Templates.
+ */
+public record NewUserRecord(String username, String password, String firstName, String lastName) {
+}
