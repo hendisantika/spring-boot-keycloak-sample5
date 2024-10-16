5c63bb1270fe8cc7f0ec2a4f8d28a7e0226ca920
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:26:05 +0700

    Add RoleService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaksample5/service/RoleService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.keycloaksample5.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/10/24
+ * Time: 14.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+}
