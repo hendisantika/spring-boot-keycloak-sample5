3f06d36e448259b384e1d49fba14918eb72eb64f
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:39:08 +0700

    Add testForMergeRole in RBACController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample5/controller/RBACController.java
+++ src/main/java/id/my/hendisantika/keycloaksample5/controller/RBACController.java
@@ -1,6 +1,11 @@
 package id.my.hendisantika.keycloaksample5.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 14.37
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
 @RequestMapping("/rbac")
 @RequiredArgsConstructor
 public class RBACController {
-
+    @GetMapping("merge-role")
+    @PreAuthorize("hasAnyRole('MERGE')")
+    public ResponseEntity<?> testForMergeRole() {
+        log.info("SOMETHING ");
+        return ResponseEntity.status(HttpStatus.OK).body("I HAVE ACCESS to MERGE ROLE");
+    }
 }
