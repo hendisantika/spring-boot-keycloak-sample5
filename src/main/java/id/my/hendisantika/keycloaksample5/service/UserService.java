9c7d525f1bf9af24ee5472e608b7644d9aa8cffd
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:17:40 +0700

    Add createUser in UserService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample5/service/UserService.java
+++ src/main/java/id/my/hendisantika/keycloaksample5/service/UserService.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.keycloaksample5.service;
 
+import id.my.hendisantika.keycloaksample5.model.NewUserRecord;
+import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +31,37 @@ public class UserService {
     private final Keycloak keycloak;
     @Value("${app.keycloak.realm}")
     private String realm;
+
+    public void createUser(NewUserRecord newUserRecord) {
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setEnabled(true);
+        userRepresentation.setFirstName(newUserRecord.firstName());
+        userRepresentation.setLastName(newUserRecord.lastName());
+        userRepresentation.setUsername(newUserRecord.username());
+        userRepresentation.setEmail(newUserRecord.username());
+        userRepresentation.setEmailVerified(false);
+
+        CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+        credentialRepresentation.setValue(newUserRecord.password());
+        credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+
+        userRepresentation.setCredentials(List.of(credentialRepresentation));
+
+        UsersResource usersResource = getUsersResource();
+
+        Response response = usersResource.create(userRepresentation);
+
+        log.info("Status Code " + response.getStatus());
+
+        if (!Objects.equals(201, response.getStatus())) {
+
+            throw new RuntimeException("Status code " + response.getStatus());
+        }
+
+        log.info("New user has bee created");
+
+        List<UserRepresentation> userRepresentations = usersResource.searchByUsername(newUserRecord.username(), true);
+        UserRepresentation userRepresentation1 = userRepresentations.get(0);
+        sendVerificationEmail(userRepresentation1.getId());
+    }
 }
