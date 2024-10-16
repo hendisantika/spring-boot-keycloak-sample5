ded517792e78e8ee07c53f86cb5ec506f0b52cdc
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:24:58 +0700

    Add webSecurityCustomizer in WebSecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample5/config/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/keycloaksample5/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.keycloaksample5.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +24,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> {
+            web.ignoring().requestMatchers(
+                    HttpMethod.POST,
+                    "/public/**",
+                    "/users"
+            );
+            web.ignoring().requestMatchers(
+                    HttpMethod.GET,
+                    "/public/**"
+            );
+            web.ignoring().requestMatchers(
+                    HttpMethod.DELETE,
+                    "/public/**",
+                    "/users/{id}"
+            );
+            web.ignoring().requestMatchers(
+                    HttpMethod.PUT,
+                    "/public/**",
+                    "/users/{id}/send-verification-email",
+                    "/users/forgot-password"
+
+            );
+            web.ignoring().requestMatchers(
+                            HttpMethod.OPTIONS,
+                            "/**"
+                    )
+                    .requestMatchers("/v3/api-docs/**", "/configuration/**", "/swagger-ui/**",
+                            "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/api-docs/**");
+
+        };
+    }
 }
