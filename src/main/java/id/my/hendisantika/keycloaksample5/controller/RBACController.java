a2fa72faff1d7991c69fd3d997b85ca32e47f1f7
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:37:54 +0700

    Add RBACController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaksample5/controller/RBACController.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.keycloaksample5.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/10/24
+ * Time: 14.37
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/rbac")
+@RequiredArgsConstructor
+public class RBACController {
+
+}
