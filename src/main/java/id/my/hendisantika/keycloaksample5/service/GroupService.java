e0835848e43905d21feb4ea2c333c16c8ca95f7e
hendisantika <hendisantika@yahoo.co.id>
2024-10-16 14:28:29 +0700

    Add assignGroup in GroupService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample5/service/GroupService.java
+++ src/main/java/id/my/hendisantika/keycloaksample5/service/GroupService.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.keycloaksample5.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.UserResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -27,4 +28,9 @@ public class GroupService {
     @Value("${app.keycloak.realm}")
     private String realm;
 
+    public void assignGroup(String userId, String groupId) {
+        UserResource user = userService.getUser(userId);
+        user.joinGroup(groupId);
+        log.info("User joined group {}", groupId);
+    }
 }
